787726330c4fff7fbd86fefe657b6c34597007a7
s194050@student.dtu.dk <s194050@student.dtu.dk>
2022-02-23 12:32:36 +0100

    FIFO design

--- hardware/src/main/scala/io/FIFO.scala
+++ hardware/src/main/scala/io/FIFO.scala
@@ -12,44 +12,46 @@ object FIFO extends DeviceObject {
 
   def create(params: Map[String, String]): FIFO = Module(new FIFO())
 }
+// Temp FIFO in chisel3 to be written in VHDL for final version
 
-class FIFO() extends CoreDevice() {
+class FIFO() extends CoreDevice() { // Create DecoupledIO, which is a bundle with ready-valid interface
 override val io = IO(new CoreDeviceIO(){
-  val enq = Flipped(new DecoupledIO(42.U))
-  val deq = new DecoupledIO(42.U)
+  val enq = Flipped(new DecoupledIO(32.U)) //Writer to FIFO
+  val deq = new DecoupledIO(32.U) // Reader to FIFO
 })
 
-  val fullReg = RegInit(false.B)
-  val dataReg = Reg(42.U)
+  val masterReg = Reg(next = io.ocp.M) // To use OCPcore
+  val emptyReg = RegInit(true.B)
+  val fullReg = RegInit(false.B) // Bool to signal whether FIFO is full
+  val dataReg = Reg(32.U)
 
-  when (fullReg) {
-    when (io.deq.ready) {
-      fullReg := false.B
-    }
-  } .otherwise {
-    when (io.enq.valid) {
+  // Default response
+  val respReg = Reg(init = OcpResp.NULL)
+  respReg := OcpResp.NULL
+
+  when(masterReg.Cmd === OcpCmd.WR){ // Write to FIFO
+    when(io.enq.valid && !fullReg){
+      respReg := OcpResp.DVA
+      io.enq.bits := masterReg.Data
+      emptyReg := false.B
       fullReg := true.B
       dataReg := io.enq.bits
     }
   }
+
+  when(masterReg.Cmd === OcpCmd.RD){ // Read from FIFO
+    when(io.deq.ready && !emptyReg){
+      respReg := OcpResp.DVA
+      fullReg := false.B
+      emptyReg := true.B
+    }
+  }
+
   io.enq.ready := !fullReg
-  io.deq.valid := fullReg
+  io.deq.valid := !emptyReg
   io.deq.bits := dataReg
 
-
-
-  val countReg = Reg(init = UInt(0, 32))
-  countReg := countReg + UInt(1)
-  when (io.ocp.M.Cmd === OcpCmd.WR) {
-    countReg := io.ocp.M.Data
-  }
-
-  val respReg = Reg(init = OcpResp.NULL)
-  respReg := OcpResp.NULL
-  when(io.ocp.M.Cmd === OcpCmd.RD || io.ocp.M.Cmd === OcpCmd.WR) {
-    respReg := OcpResp.DVA
-  }
-
-  io.ocp.S.Data := countReg
+  // Connections to master
   io.ocp.S.Resp := respReg
+  io.ocp.S.Data := io.deq.bits
 }
