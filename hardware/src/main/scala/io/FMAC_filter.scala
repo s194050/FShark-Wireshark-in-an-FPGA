67f2d46dbf9549e23771dd6957258e826d61f8f7
William Mathiassen <williammathiassen@pop-os.localdomain>
2022-04-03 12:37:58 +0200

    Preliminary simple filter logic

--- hardware/src/main/scala/io/FMAC_filter.scala
+++ hardware/src/main/scala/io/FMAC_filter.scala
@@ -9,5 +9,43 @@ class FMAC_filter extends  Module{
       val io = IO(new Bundle{
         val in = Module(new FMAC)
       })
+  // Counter to count the amount of bytes in each frame recieved
+  val cntFrame = RegInit(0.U(32.W))
+  val filteredFrame = WireInit(true.B)
+
+ // If ctl is high, frame is recieved, increase counter at each
+  // rising edge, as a byte is transmitted each rising edge
+  when(io.in.io.pins.rgmii_rx_ctl){
+    cntFrame := cntFrame + io.in.io.pins.rgmii_rx_clk
+  }
+
+  // When t_last is high the frame has reached the end. reset counter
+  when(io.in.ethmac1g.io.rx_axis_tlast){
+    cntFrame := 0.U
+  }
+
+  // Check if frame[18] is the correct value otherwise filter it out
+  when(cntFrame === 18.U){
+    when(io.in.ethmac1g.io.rx_axis_tdata === 0x18.U){
+      filteredFrame := false.B
+    }
+  }
+  // FSM to determine whether frame data in buffer is kept or flushed
+  val bufferIdle :: bufferFlush :: bufferOkay :: Nil = Enum(3)
+  val stateBuffer = RegInit(bufferIdle)
+
+  when(stateBuffer === bufferIdle){
+    when(!filteredFrame) {
+      stateBuffer := bufferFlush
+    }.otherwise{
+      stateBuffer := bufferOkay
+    }
+  }
+  when(stateBuffer === bufferFlush){
+    // Flush the buffer
+  }
+  when(stateBuffer === bufferOkay){
+    // Keep writing to buffer
+  }
 
 }
