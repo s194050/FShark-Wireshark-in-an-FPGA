3e860163b4e83d0b94236c335c0ff73ff67262bf
William <s194050@student.dtu.dk>
2022-05-03 10:49:27 +0200

    Fix PHY clocks not connected

--- hardware/vhdl/patmos_de2-115.vhdl
+++ hardware/vhdl/patmos_de2-115.vhdl
@@ -203,10 +203,10 @@ begin
     );
 
   -- Ethernet Pass-through
-  ENET1_GTX_CLK <= ENET0_RX_CLK;
+  ENET1_GTX_CLK <= clk_125;
   ENET1_TX_DATA <= ENET0_RX_DATA;
   ENET1_TX_EN <= ENET0_RX_DV;
-  ENET0_GTX_CLK <= ENET1_RX_CLK;
+  ENET0_GTX_CLK <= clk_125;
   ENET0_TX_DATA <= ENET1_RX_DATA;
   ENET0_TX_EN <= ENET1_RX_DV;
 
